3668e1d266380d159c64e7a05ec3c100078312e4
sleepysadkid <lizheqing16@gmail.com>
2018-10-30 20:10:51 -0700

    layout update

--- app/src/main/java/com/group5/android_project/MainActivity.java
+++ app/src/main/java/com/group5/android_project/MainActivity.java
@@ -3,39 +3,91 @@ package com.group5.android_project;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
+import com.group5.android_project.fragment.PostFragment;
+import com.group5.android_project.fragment.ProfileFragment;
+import com.group5.android_project.fragment.SearchFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    PostFragment postFragment;
+    ProfileFragment profileFragment;
+    SearchFragment searchFragment;
+    MenuItem prevMenuItem;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewpager);
+        bottomNavigationView = findViewById(R.id.navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.action_search:
+                                viewPager.setCurrentItem(0);
+                                break;
+                            case R.id.action_post:
+                                viewPager.setCurrentItem(1);
+                                break;
+                            case R.id.action_profile:
+                                viewPager.setCurrentItem(2);
+                                break;
+                        }
+                        return false;
+                    }
+                }
+        );
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                } else {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                Log.d("page", "onPageSelected: " + i);
+                bottomNavigationView.getMenu().getItem(i).setChecked(true);
+                prevMenuItem = bottomNavigationView.getMenu().getItem(i);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+        setupViewPager(viewPager);
+
     }
 
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        postFragment = new PostFragment();
+        profileFragment = new ProfileFragment();
+        searchFragment = new SearchFragment();
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        adapter.addFragment(searchFragment);
+        adapter.addFragment(postFragment);
+        adapter.addFragment(profileFragment);
 
-            Fragment fragment = null;
-
-            switch (item.getItemId()) {
-                case R.id.action_search:
-
-                    return true;
-                case R.id.action_post:
-
-                    return true;
-                case R.id.action_profile:
-
-                    return true;
-            }
-            return false;
-        }
-    };
+        viewPager.setAdapter(adapter);
+    }
 }
---
+++ app/src/main/java/com/group5/android_project/fragment/SearchFragment.java
@@ -0,0 +1,23 @@
+package com.group5.android_project.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.group5.android_project.R;
+
+public class SearchFragment extends Fragment {
+
+    public SearchFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_search, container, false);
+    }
+
+}
