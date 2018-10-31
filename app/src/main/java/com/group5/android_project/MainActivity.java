db8c495f52f22df893c3b4a037b37d491bbd4b52
sleepysadkid <lizheqing16@gmail.com>
2018-10-30 17:19:07 -0700

    bottom navigation layout

--- app/src/main/java/com/group5/android_project/MainActivity.java
+++ app/src/main/java/com/group5/android_project/MainActivity.java
@@ -1,7 +1,11 @@
 package com.group5.android_project;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment fragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.action_search:
+
+                    return true;
+                case R.id.action_post:
+
+                    return true;
+                case R.id.action_profile:
+
+                    return true;
+            }
+            return false;
+        }
+    };
 }
